23417b970224a96dd9d1b48c815112cc120b838f
mwrkhaeriza <mawar011208@gmail.com>
2025-08-05 13:16:28 +0700

    Add files via upload

---
+++ ProjectBaruBab7.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project.baru.bab.pkg7;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProjectBaruBab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        //cara 1
+//        String[] nama1;
+//        
+//        //cara 2 string
+//        String[] nama2;
+//        //cara3
+//        String[] nama3 = new String[2];
+//String nama[] = new String[3];
+//nama[0] = "Budi";
+//nama[1] = "Andi";
+//nama[2] = "Dwi";
+//
+//System.out.println("Index 0 : "+nama[0]);
+//System.out.println("Index 1 : "+nama[1]);
+//System.out.println("Index 2 : "+nama[2]);
+//{ String[] nama = new
+//                String[3];
+//nama[0] = JOptionPane.showInputDialog(null, "Masukkan nama ke-0:");
+//        nama[1] = JOptionPane.showInputDialog(null, "Masukkan nama ke-1:");
+//        nama[2] = JOptionPane.showInputDialog(null, "Masukkan nama ke-2:");
+//
+//        JOptionPane.showMessageDialog(null, 
+//            "Nama pada index ke-0: " + nama[0] + "\n" +
+//            "Nama pada index ke-1: " + nama[1] + "\n" +
+//            "Nama pada index ke-2: " + nama[2]);
+//String umur[][] = new String[2][2];
+//umur[0][0] = "Budi";
+//umur[0][1] = "20";
+//umur[1][0] = "Andi";
+//umur[1][1] = "45";
+//
+//System.out.println("Nama : "+umur[0][0]+" umur : "+umur[0][1]);
+//System.out.println("Nama : "+umur[1][0]+" umur : "+umur[1][1]);
+        }
+
+    }
+    
+
